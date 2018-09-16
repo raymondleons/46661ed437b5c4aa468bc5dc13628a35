ccd9f6e9cd7366cf32406db1149a9a0930dd7acc
RomanFals <fals.roman@gmail.com>
2018-09-16 17:34:14 +0700

    perbaiki index

--- index.php
+++ index.php
@@ -11,13 +11,8 @@
         <link rel="shortcut icon" href="<?= base_url('assets/template/images/favicon.ico'); ?>">
 
         <!-- App title -->
-<<<<<<< HEAD
         <title>Pemira - In Develop</title>
-        qwerty
-=======
-        <title>Pemira - In Developasdasdasd</title>
 
->>>>>>> 3f1540c4b37dfa77f46f4eeabd8ff62144ece009
         <!-- App CSS -->
         <link href="<?= base_url('assets/template/css/style.css'); ?>" rel="stylesheet" type="text/css" />
 
